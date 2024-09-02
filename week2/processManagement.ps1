8f76d59794e4dddf103a466237e3e0703ff9e879
Laura <laura.thomas-roy@mymail.champlain.edu>
2024-09-02 12:07:49 -0400

    second week assignment

---
+++ week2/processManagement.ps1
@@ -0,0 +1,15 @@
+﻿clear
+
+# Q1
+#Get-Process | Where-object { $_.Name -ilike "C*" }
+
+# Q2
+#Get-Process | Where-object { $_.Path -notlike "*system32*" } | select Path
+
+# Q3
+#Get-Service | select Name, Status | Where-Object {$_.Status -eq "Stopped"} | `
+ #Export-Csv -Path C:\Users\champuser\Desktop\CSI-230/week2\Processes.csv -NoTypeInformation 
+
+
+# Q4
+if (Get-Process chrome){Stop-Process -name chrome} else {Start-Process chrome -ArgumentList champlain.edu}
