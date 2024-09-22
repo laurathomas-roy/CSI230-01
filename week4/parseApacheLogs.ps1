f795e99a3c2ff6595de45efb8e0b1749813137b2
laurathomas-roy <143466475+laurathomas-roy@users.noreply.github.com>
2024-09-22 17:01:10 -0400

    Add files via upload

---
+++ week4/parseApacheLogs.ps1
@@ -0,0 +1,22 @@
+﻿function ApacheLogs1(){
+$logsNotformatted = Get-Content C:\xampp\apache\logs\access.log
+$tableRecords = @()
+
+for($i=0; $i -lt $logsNotformatted.Length; $i++){
+
+#split a string into words
+$words = $logsNotformatted[$i].Split(" ");
+
+  $tableRecords += [pscustomobject]@{ "IP" = $words[0]; `
+                                      "Time" = $words[3].Trim('['); `
+                                      "Method" = $words[5].Trim('"'); `
+                                      "Page" = $words[6]; `
+                                      "Protocol" = $words[7]; `
+                                      "Response" = $words[8]; `
+                                      "Referrer" = $words[10]; `
+                                      "Client" = $words[11]; }
+} #end of for loop
+return $tableRecords | Where-Object { $_.IP -like "10.*" }
+}
+$tableRecords = ApacheLogs1
+$tableRecords | Format-Table -AutoSize -Wrap
