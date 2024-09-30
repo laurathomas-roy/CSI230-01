999ee4b844ef9b58622e91179f71cef569893458
laurathomas-roy <143466475+laurathomas-roy@users.noreply.github.com>
2024-09-30 12:45:33 -0400

    week six assignment

---
+++ week6/String-Helper.ps1
@@ -0,0 +1,48 @@
+﻿<# String-Helper
+*************************************************************
+   This script contains functions that help with String/Match/Search
+   operations. 
+************************************************************* 
+#>
+
+
+<# ******************************************************
+   Functions: Get Matching Lines
+   Input:   1) Text with multiple lines  
+            2) Keyword
+   Output:  1) Array of lines that contain the keyword
+********************************************************* #>
+function getMatchingLines($contents, $lookline){
+
+$allines = @()
+$splitted =  $contents.split([Environment]::NewLine)
+
+for($j=0; $j -lt $splitted.Count; $j++){  
+ 
+   if($splitted[$j].Length -gt 0){  
+        if($splitted[$j] -ilike $lookline){ $allines += $splitted[$j] }
+   }
+
+}
+
+return $allines
+}
+
+
+<# ******************************
+# Create a function that checks if password meets certain parameters
+****************************** #>
+function checkPassword($password){
+    $bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
+
+    $plainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($bstr)
+
+    #at least 6 chars
+    if($plainPassword.Length -ge 6){
+        #one special char, one number, one letter
+        if(($plainPassword -match "[^a-zA-Z0-9]") -and ($plainPassword -match "[0-9]") -and ($plainPassword -match "[a-zA-Z]")){
+            return $true
+        }
+    }
+    return $false
+}
---
+++ week6/main-1.ps1
@@ -0,0 +1,172 @@
+﻿. (Join-Path $PSScriptRoot Users.ps1)
+. (Join-Path $PSScriptRoot Event-Logs.ps1)
+
+clear
+
+$Prompt = "`n"
+$Prompt += "Please choose your operation:`n"
+$Prompt += "1 - List Enabled Users`n"
+$Prompt += "2 - List Disabled Users`n"
+$Prompt += "3 - Create a User`n"
+$Prompt += "4 - Remove a User`n"
+$Prompt += "5 - Enable a User`n"
+$Prompt += "6 - Disable a User`n"
+$Prompt += "7 - Get Log-In Logs`n"
+$Prompt += "8 - Get Failed Log-In Logs`n"
+$Prompt += "9 - List at Risk Users`n"
+$Prompt += "10 - Exit`n"
+
+
+
+$operation = $true
+
+while($operation){
+
+    
+    Write-Host $Prompt | Out-String
+    $choice = Read-Host 
+
+
+    if($choice -eq 10){
+        Write-Host "Goodbye" | Out-String
+        exit
+        $operation = $false 
+    }
+
+    elseif($choice -eq 1){
+        $enabledUsers = getEnabledUsers
+        Write-Host ($enabledUsers | Format-Table | Out-String)
+    }
+
+    elseif($choice -eq 2){
+        $notEnabledUsers = getNotEnabledUsers
+        Write-Host ($notEnabledUsers | Format-Table | Out-String)
+    }
+
+
+    # Create a user
+    elseif($choice -eq 3){ 
+
+        $name = Read-Host -Prompt "Please enter the username for the new user"
+        $password = Read-Host -AsSecureString -Prompt "Please enter the password for the new user"
+
+        $exists = checkUser($name)
+        if($exists){
+            Write-Host "User $name already exists."
+            exit
+        }
+
+        $pwGood = checkPassword($password)
+        if(!$pwGood){
+            Write-Host "Invalid Password."
+            exit
+        }
+    
+        createAUser $name $password
+
+        Write-Host "User: $name is created." | Out-String
+    }
+
+
+    # Remove a user
+    elseif($choice -eq 4){
+
+        $name = Read-Host -Prompt "Please enter the username for the user to be removed"
+
+        $exists = checkUser($name)
+        if(!$exists) {
+            Write-Host "User does not exist."
+            exit
+        }
+
+        removeAUser $name
+
+        Write-Host "User: $name Removed." | Out-String
+    }
+
+
+    # Enable a user
+    elseif($choice -eq 5){
+
+
+        $name = Read-Host -Prompt "Please enter the username for the user to be enabled"
+
+        $exists = checkUser($name)
+        if(!$exists) {
+            Write-Host "User does not exist."
+            exit
+        }
+
+        enableAUser $name
+
+        Write-Host "User: $name Enabled." | Out-String
+    }
+
+
+    # Disable a user
+    elseif($choice -eq 6){
+
+        $name = Read-Host -Prompt "Please enter the username for the user to be disabled"
+
+        $exists = checkUser($name)
+        if(!$exists) {
+            Write-Host "User does not exist."
+            exit
+        }
+
+        disableAUser $name
+
+        Write-Host "User: $name Disabled." | Out-String
+    }
+
+
+    elseif($choice -eq 7){
+
+        $name = Read-Host -Prompt "Please enter the username for the user logs"
+
+        $exists = checkUser($name)
+        if(!$exists) {
+            Write-Host "User does not exist."
+            exit
+        }
+
+        $days = Read-Host -Prompt "Please enter the number of days you would like logs for"
+        $userLogins = getLogInAndOffs $days
+
+        Write-Host ($userLogins | Where-Object { $_.User -ilike "*$name"} | Format-Table | Out-String)
+    }
+
+
+    elseif($choice -eq 8){
+
+        $name = Read-Host -Prompt "Please enter the username for the user's failed login logs"
+
+        $exists = checkUser($name)
+        if(!$exists) {
+            Write-Host "User does not exist."
+            exit
+        }
+
+        $days = Read-Host -Prompt "Please enter the number of days you would like logs for"
+        $userLogins = getFailedLogins $days #does this need to be changed?
+
+        Write-Host ($userLogins | Where-Object { $_.User -ilike "*$name"} | Format-Table | Out-String)
+    }
+
+
+    elseif($choice -eq 9){
+        $days = Read-Host -Prompt "Please enter the number of days you would like logs for"
+        $userLogins = getFailedLogins $days
+        $loginsByUser = $userLogins | Group-Object "User" `
+                                    | Select-Object Name, Count `
+                                    | Where-Object { $_.Count -ge 10 }
+              
+        Write-Host ($loginsByUser | Format-Table | Out-String)
+
+    }
+
+
+    else {
+        Write-Host "Invalid request. Please try again."
+    }
+}
